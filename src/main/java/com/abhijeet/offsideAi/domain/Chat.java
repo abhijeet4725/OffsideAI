b57113bcd67621a78bfe8eefe7cd513b2c448ff7
abhijeet4725 <abhijeetcs4725@gmail.com>
2025-08-11 17:24:35 +0530

    feat:application.properties created and DB, JPA configuration added

--- src/main/java/com/abhijeet/offsideAi/domain/Chat.java
+++ src/main/java/com/abhijeet/offsideAi/domain/Chat.java
@@ -1,9 +1,7 @@
 package com.abhijeet.offsideAi.domain;
 
-import io.micrometer.core.annotation.Counted;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.cglib.core.Local;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -45,8 +43,6 @@ public class Chat {
     @Column(name = "updated_at", nullable = false)
     @LastModifiedDate
     private LocalDateTime updatedAt;
-
-
 
 
 }
